e1b74ca0575f59d2cae92b80bbcf720136427ee8
sasa <blackwind99m16@gmail.com>
2026-04-06 15:01:08 +0200

    4th_commit

--- .ipynb_checkpoints/file-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-# Check Java version
-result = subprocess.run(['java', '-version'], capture_output=True, text=True)
-print(result.stderr)
-
-# Check PySpark version
-import pyspark
-print("PySpark version:", pyspark.__version__)
